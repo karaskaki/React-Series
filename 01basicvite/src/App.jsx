ac37782c887f79a84c1ba66d8e9533340bff08b0
Nikhil Singh <95135482+karaskaki@users.noreply.github.com>
2023-12-28 12:52:02 +0530

    Custom React

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1>Vite React app</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ customReact/customreact.js
@@ -0,0 +1,20 @@
+function mainContainer(reactElement, container) {
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'h1',
+    props: {
+        href: "https//google.com",
+        target: "_blank"
+    },
+    children: "click me to visit google"
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer) 
